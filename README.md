45c0ad9f4e38ad1532f2946ec9747d5b6093b0ef
green <vgrinyov@gmail.com>
2017-01-18 23:29:21 +0200

    init commit

---
+++ README.md
@@ -0,0 +1,1 @@
+"# csv_rest_parser" 
---
+++ src/main/java/com/grinyov/csv_rest_parser/Application.java
@@ -0,0 +1,21 @@
+package com.grinyov.csv_rest_parser;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by vgrinyov.
+ */
+
+@SpringBootApplication
+public class Application implements CommandLineRunner {
+
+    @Override
+    public void run(String... args) throws Exception {
+
+       /* String cityName = args[0].trim();
+        String fileName = cityName + ".csv";
+
+        data = someSiteApiClient.find*/
+    }
+}
