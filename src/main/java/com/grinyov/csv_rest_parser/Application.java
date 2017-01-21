f838a92247d1eadb57e561db2bb712ba0439f8ca
green <vgrinyov@gmail.com>
2017-01-21 11:25:11 +0200

    Add set start's data

--- src/main/java/com/grinyov/csv_rest_parser/Application.java
+++ src/main/java/com/grinyov/csv_rest_parser/Application.java
@@ -1,7 +1,10 @@
 package com.grinyov.csv_rest_parser;
 
+import com.google.common.collect.ImmutableList;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import static java.util.stream.Collectors.toList;
 
 /**
  * Created by vgrinyov.
@@ -13,9 +16,19 @@ public class Application implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
 
-       /* String cityName = args[0].trim();
+        String cityName = args[0].trim();
         String fileName = cityName + ".csv";
 
-        data = someSiteApiClient.find*/
+        /*  Call csv writer.
+           FIrstly, apiclient's method call and receive data in the form map.
+           Call method csvSuggestionConverter transform to dto and
+           move to new immutable collection.
+           Finally, save to file in *.csv format
+        */
+
+        csvSuggestWriter.write(fileName, someSiteApiClient.findSuggestionsByCity().stream()
+                .map(csvSuggestionConverter::toCsvSuggestionDto)
+                .collect(collectingAndThen(toList(), ImmutableList::copyOf)));
+
     }
 }
