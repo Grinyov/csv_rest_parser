bc8a6629322d1b1621d4b55840f4a2ad34b2fcd9
green <vgrinyov@gmail.com>
2017-01-21 19:32:11 +0200

    add application properties file

--- src/main/java/com/grinyov/csv_rest_parser/service/WebsiteApiClient.java
+++ src/main/java/com/grinyov/csv_rest_parser/service/WebsiteApiClient.java
@@ -1,14 +1,29 @@
 package com.grinyov.csv_rest_parser.service;
 
+import com.grinyov.csv_rest_parser.dao.model.Suggestion;
 import lombok.NonNull;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
 
 /**
  * Created by vgrinyov.
  */
 @Component
 public class WebsiteApiClient {
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    @Value("${application.suggestionUrlTemplate}")
+    private String suggestionUrl;
+
     public List<Suggestion> findSuggestionsByCity(@NonNull String city){
+
+        restTemplate.getForEntity(suggestionUrl, Suggestion[].class,  )
         return null;
     }
 }
